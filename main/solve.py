fe411e408ff1e812afbdc2ef3c8ea467328ba921
Clement Roig <clement.roig@tutanota.com>
2018-03-01 23:21:19 +0100

    Tmp

--- main/solve.py
+++ main/solve.py
@@ -47,9 +47,9 @@ def solve(data, load, callback, time, **args):
 
     # Constraints
     # model.constraint(model.disjoint(cars))
-    # for late, car in zip(lates, cars):
-    #   for i in range(N):
-    #     model.constraint(i >= model.count(car) or late[i] <= model.at(max_lates, car[i]))
+    for late, car in zip(lates, cars):
+      for i in range(N):
+        model.constraint(late[i] <= model.at(max_lates, car[i]))
 
     # Objective
     model.maximize(model.sum([model.count(car) for car in cars]))
