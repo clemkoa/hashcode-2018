9858ce35923a2fc8e9340da62b03d1a6736f9ee2
Clement Roig <clement.roig@tutanota.com>
2018-03-01 21:08:34 +0100

    Tmp

--- main/solve.py
+++ main/solve.py
@@ -42,11 +42,7 @@ def solve(data, load, callback, time, **args):
     cars = [model.list(N) for i in range(F)]
 
     # Expressions
-    fns = [model.function(lambda i, prev: \
-      # prev if i == 0 else model.max(0, prev) + \
-      prev + \
-      model.at(times, 0, car[0] + 1) if i == 0 else \
-      model.at(times, car[i] + 1, car[i+1] + 1)) for car in cars]
+    fns = [model.function(lambda i, prev: prev + (model.at(times, 0, car[0] + 1) if i == 0 else model.at(times, car[i] + 1, car[i+1] + 1))) for car in cars]
     lates = [model.array(model.range(0, N), fn) for fn, car in zip(fns, cars)]
 
     # Constraints
