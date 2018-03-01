ce19dfc6ec7bb2f2b4b2d01341d81b0103719213
Clement Roig <clement.roig@tutanota.com>
2018-03-01 22:35:09 +0100

    Tmp

--- main/solve.py
+++ main/solve.py
@@ -38,12 +38,14 @@ def solve(data, load, callback, time, **args):
     max_lates = model.array([e - s for _, _, s, e in demand])
 
     # Variables
-    cars = [model.list(N) for i in range(F)]
+    cars = model.array([model.list(N) for i in range(F)])
 
     # Expressions
     def make_lambda(k):
-      return lambda i, prev: model.max(0, prev + (model.at(times, 0, cars[k][0] + 1) if i == 0 else model.at(times, cars[k][i-1] + 1, cars[k][i] + 1)))
-    lates = model.array(model.range(0, F), model.function(lambda k: model.array(model.range(0, N), model.function(make_lambda(k)))))
+      return lambda i, prev: model.max(0, prev + \
+        (model.at(times, 0, model.at(cars, k, 0) + 1) if i == 0 else \
+        model.at(times, model.at(cars, k, i-1) + 1, model.at(cars, k, i) + 1)))
+    lates = model.array(model.range(0, F), model.function(lambda k: model.array(model.range(0, N), model.function(make_lambda))))
 
     # Constraints
     model.constraint(model.disjoint(cars))
