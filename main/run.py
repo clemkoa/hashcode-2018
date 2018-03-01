8ba339f1dc073852b4e5faf2803b905e475bc5a9
Paul Mustiere <pmustiere@palantir.com>
2018-03-01 21:35:21 +0100

    nit

--- main/run.py
+++ main/run.py
@@ -96,8 +96,8 @@ def run(**args):
             # Add this ride to this car
             rides_todo[index_ride] = False
             solution[car].append(index_ride)
-            # Update car's position and next available time
 
+            # Update car's position and next available time
             positions[car] = (x, y)
             free_cars[car] = time_step + time_to_go_to_rides[index_ride] + ride_times[index_ride]
 
